05a6e5c58ba946089c14b2e465fb0371d4a7a692
Lalitwadee Singsoot <56551987+Lalitwadee@users.noreply.github.com>
2022-08-11 00:36:39 +0700

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -10,3 +10,57 @@ resource "aws_iam_user_policy_attachment" "attach-user" {
   user       = "${aws_iam_user.iam-rayrai.name}"
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
+
+#----------------------------------------------------- Create iam role : cloud_trail
+ 
+resource "aws_iam_role" "cloud_trail" {
+  name = "cloudTrail-cloudWatch-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "role-policy-ctcw" {
+  name = "cloudTrail-cloudWatch-policy"
+  role = aws_iam_role.cloud_trail.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailCreateLogStream2014110",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream"
+            ],
+            "Resource": [
+                "${aws_cloudwatch_log_group.s3bk.arn}:*"
+            ]
+        },
+        {
+            "Sid": "AWSCloudTrailPutLogEvents20141101",
+            "Effect": "Allow",
+            "Action": [
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                "${aws_cloudwatch_log_group.s3bk.arn}:*"
+            ]
+        }
+    ]
+}
+EOF
+}
