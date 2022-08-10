bdced25063d8a5bcf13601b06c2d963b95e8c34a
Lalitwadee Singsoot <56551987+Lalitwadee@users.noreply.github.com>
2022-08-10 17:20:49 +0700

    Update s3-ap.tf

--- s3-ap.tf
+++ s3-ap.tf
@@ -4,9 +4,9 @@
 # TEST  : (Download File ) aws s3 cp s3://arn:aws:s3:us-east-1:747913987992:accesspoint/ap-ray/1.png /Users/rari3/Downloads/1.png
 # ----------------------------------------------------------------------------------------------------------------------------------------------
 
-resource "aws_s3_access_point" "user2-ap" {
+resource "aws_s3_access_point" "user-ap" {
   bucket = aws_s3_bucket.s3.bucket
-  name   = "${var.ap2-name}"
+  name   = "${var.ap-name}"
 
   public_access_block_configuration {
     block_public_acls       = true
@@ -20,8 +20,8 @@ resource "aws_s3_access_point" "user2-ap" {
   }
 }
 
-resource "aws_s3control_access_point_policy" "ap2-policy" {
-  access_point_arn = aws_s3_access_point.user2-ap.arn
+resource "aws_s3control_access_point_policy" "ap-policy" {
+  access_point_arn = aws_s3_access_point.user-ap.arn
 
   policy = jsonencode({
     "Version":"2012-10-17",
@@ -32,7 +32,7 @@ resource "aws_s3control_access_point_policy" "ap2-policy" {
             "AWS": "arn:aws:iam::${var.account_id}:user/${var.iam-user}"
         },
         "Action": "s3:PutObject",
-        "Resource": "arn:aws:s3:${var.region}:${var.account_id}:accesspoint/${var.ap2-name}/object/*"
+        "Resource": "arn:aws:s3:${var.region}:${var.account_id}:accesspoint/${var.ap-name}/object/*"
     }]
 })
 }
