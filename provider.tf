d645f5c2060591a93d74c0c9a31ade70d5ec53a2
Lalitwadee Singsoot <56551987+Lalitwadee@users.noreply.github.com>
2022-08-10 17:02:30 +0700

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+# Region in which S3 bucket will create
+
+provider "aws" {
+  region  = "${var.region}"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
